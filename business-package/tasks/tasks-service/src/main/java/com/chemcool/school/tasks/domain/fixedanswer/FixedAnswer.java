4adb455e1569633984dd0eb0afdf361f12f33c71
irelion <irelion@mail.ru>
2021-01-06 22:26:27 +0300

    Добавлен Lombok, статический метод генерации FixedAnswer.

--- business-package/tasks/tasks-service/src/main/java/com/chemcool/school/tasks/domain/fixedanswer/FixedAnswer.java
+++ business-package/tasks/tasks-service/src/main/java/com/chemcool/school/tasks/domain/fixedanswer/FixedAnswer.java
@@ -1,13 +1,24 @@
 package com.chemcool.school.tasks.domain.fixedanswer;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "fixedAnswer")
 public class FixedAnswer {
 
+    public static FixedAnswer getInstance() {
+        return new FixedAnswer();
+    }
+
     @Id
-    private Long id;
+    private String id;
 
     @Column(name = "description")
     private String description;
@@ -19,48 +30,5 @@ public class FixedAnswer {
     private int numberOfChapter;
 
     @Column(name = "typeOfTasks")
-    private Enum typeOfTasks;
-
-    public FixedAnswer() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getRightAnswer() {
-        return rightAnswer;
-    }
-
-    public void setRightAnswer(String rightAnswer) {
-        this.rightAnswer = rightAnswer;
-    }
-
-    public int getNumberOfChapter() {
-        return numberOfChapter;
-    }
-
-    public void setNumberOfChapter(int numberOfChapter) {
-        this.numberOfChapter = numberOfChapter;
-    }
-
-    public Enum getTypeOfTasks() {
-        return typeOfTasks;
-    }
-
-    public void setTypeOfTasks(Enum typeOfTasks) {
-        this.typeOfTasks = typeOfTasks;
-    }
+    private TypeOfTask typeOfTasks;
 }
