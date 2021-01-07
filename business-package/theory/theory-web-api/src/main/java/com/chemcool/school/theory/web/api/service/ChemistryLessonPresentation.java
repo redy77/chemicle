e7fbb4e7f5f5a494be895fbce59b2afb7a352798
Azamat <Xrupsarius0505>
2021-01-07 22:14:22 +0300

    Добавил сваггер, чтобы была возможность с удобством добавлять темы и проверять их.

--- business-package/theory/theory-web-api/src/main/java/com/chemcool/school/theory/web/api/service/ChemistryLessonPresentation.java
+++ business-package/theory/theory-web-api/src/main/java/com/chemcool/school/theory/web/api/service/ChemistryLessonPresentation.java
@@ -3,6 +3,7 @@ package com.chemcool.school.theory.web.api.service;
 import com.chemcool.school.theory.domain.ChemistryLessonExample;
 import com.chemcool.school.theory.service.ChemistryLessonPageService;
 import com.chemcool.school.theory.web.api.dto.LessonExample;
+import com.chemcool.school.theory.web.api.exception.ChemistryLessonEmptyException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +14,22 @@ public class ChemistryLessonPresentation {
     private final ChemistryLessonPageService service;
 
     public String createExample(LessonExample example) {
-        return service.manageLesson(
-                new ChemistryLessonExample(
-                        example.getLessonExampleName(),
-                        example.getLessonExampleDescription(),
-                        example.getLessonExampleChapter(),
-                        example.getLessonExampleReferences()
-                )
-        );
+        if (
+                example.getLessonExampleName().isEmpty() ||
+                        example.getLessonExampleReferences().isEmpty() ||
+                        example.getLessonExampleChapter().isEmpty()
+        ) {
+            throw new ChemistryLessonEmptyException("Необходимы поля пустые, проверьте пожалуйста бланк заполнения темы.");
+        } else {
+            return service.manageLesson(
+                    new ChemistryLessonExample(
+                            example.getLessonExampleName(),
+                            example.getLessonExampleDescription(),
+                            example.getLessonExampleChapter(),
+                            example.getLessonExampleReferences()
+                    )
+            );
+        }
     }
 
 }
