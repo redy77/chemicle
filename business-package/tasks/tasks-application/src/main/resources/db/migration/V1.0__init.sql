77b9d52727b882e7438ae68daa0f5c3383831677
Alexander Primakov <primakovalexander07041993@gmail.com>
2021-01-15 13:27:25 +0300

    скрипт для БД 15.01

--- business-package/tasks/tasks-application/src/main/resources/db/migration/V1.0__init.sql
+++ business-package/tasks/tasks-application/src/main/resources/db/migration/V1.0__init.sql
@@ -7,7 +7,7 @@ DROP TABLE if exists MATCHING_TASK_OF_TWO_SIDES;
 
 CREATE TABLE fixed_answer
 (
-    id   VARCHAR(255)  PRIMARY KEY,
+    fixed_answer_uuid varchar(255) not null primary key,
     description    CHAR(5000) NOT NULL,
     right_answer VARCHAR(255) NOT NULL,
     chapter_id VARCHAR(255) NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE fixed_answer
 
 CREATE TABLE chemistry_equation_task
 (
-    id   VARCHAR(255) PRIMARY KEY,
+    chemistry_equation_task_uuid varchar(255) not null primary key,
     description    CHAR NOT NULL,
     right_products VARCHAR(255) NOT NULL,
     chapter_id VARCHAR(255) NOT NULL,
@@ -26,21 +26,19 @@ CREATE TABLE chemistry_equation_task
     wrong_products1 VARCHAR(255),
     wrong_products2 VARCHAR(255),
     wrong_products3 VARCHAR(255)
-
-
 )
 
 
 CREATE TABLE COMPARISON
 (
-    id   VARCHAR(255)  PRIMARY KEY,
+    id   VARCHAR(255)  varchar(255) not null primary key,
     description    VARCHAR(255) NOT NULL,
 )
 
 
 CREATE TABLE matching_task_of_two_sides
 (
-    id   VARCHAR(255)  PRIMARY KEY,
+    id   VARCHAR(255) varchar(255) not null primary key,
     description    VARCHAR(255) NOT NULL,
     chapter_id  VARCHAR(255) NOT NULL,
     type_of_tasks VARCHAR(255)
