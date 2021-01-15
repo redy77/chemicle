c8db497ca86e864e978a4732a49e4a935f73e1c0
Alexander Primakov <primakovalexander07041993@gmail.com>
2021-01-15 17:20:38 +0300

    исправил ошибки в скрипте

--- business-package/tasks/tasks-application/src/main/resources/db/migration/V1.0__init.sql
+++ business-package/tasks/tasks-application/src/main/resources/db/migration/V1.0__init.sql
@@ -1,65 +1,37 @@
--- Drop tables
-
-DROP TABLE if exists fixed_answer;
-DROP TABLE if exists choise_of_answer;
-DROP TABLE if exists matching_task_of_two_sides;
-DROP TABLE if exists couples;
-DROP TABLE if exists chemistry_equation_task;
-
-
-
-
--- Table: fixedAnswer
-CREATE TABLE fixedAnswer
-(
-    fixedAnswer_uuid   VARCHAR(255)  PRIMARY KEY,
+create table if not exists fixed_answer (
+    fixed_answer_uuid varchar(255) not null primary key,
     description    CHAR(5000) NOT NULL,
     right_answer VARCHAR(255) NOT NULL,
     chapter_id VARCHAR(255) NOT NULL,
-    type_of_tasks VARCHAR(255)
-)
+    type_of_tasks VARCHAR(255) NOT NULL
+);
 
--- Table: choise_of_answer
-CREATE TABLE choise_of_answer
+
+create table if not exists chemistry_equation_task
 (
-    choise_of_answer_uuid   VARCHAR(255) PRIMARY KEY,
+    chemistry_equation_task_uuid varchar(255) not null primary key,
     description    CHAR NOT NULL,
-    mark INT ,-- Сложность в баллах числом
-    right_answer VARCHAR(255) ,
-    incorrectAnswer1 VARCHAR(255),
-    incorrectAnswer2 VARCHAR(255),
-    incorrectAnswer3 VARCHAR(255),
-    chapter VARCHAR(255) NOT NULL
-
-)
-
--- Table: matching
-CREATE TABLE matching_task_of_two_sides
-(
-    matching_task_of_two_sides_uuid   VARCHAR(255)  PRIMARY KEY,
-    description VARCHAR(255) NOT NULL,
+    right_products VARCHAR(255) NOT NULL,
     chapter_id VARCHAR(255) NOT NULL,
-    question    VARCHAR(255) NOT NULL,
-)
+    type_of_tasks VARCHAR(255) NOT NULL,
+    reagents VARCHAR(255) NOT NULL,
+    wrong_products1 VARCHAR(255),
+    wrong_products2 VARCHAR(255),
+    wrong_products3 VARCHAR(255)
+);
 
-CREATE TABLE couples
-(
-    couples_uuid VARCHAR(255)  PRIMARY KEY,
-    left_couple VARCHAR(255) NOT NULL,
-    rightCouple VARCHAR(255) NOT NULL
-)
 
--- Table: chemistry_equation_task
-CREATE TABLE chemistry_equation_task
+create table if not exists COMPARISON
 (
-    chemistry_equation_task_uuid   VARCHAR(255)  PRIMARY KEY,
+    COMPARISON_id   varchar(255) not null primary key,
+    description    VARCHAR(255) NOT NULL
+);
+
+
+create table if not exists matching_task_of_two_sides
+(
+    matching_task_of_two_sides_id   varchar(255) not null primary key,
     description    VARCHAR(255) NOT NULL,
-    right_products    VARCHAR(255) NOT NULL,
     chapter_id  VARCHAR(255) NOT NULL,
-    type_of_tasks VARCHAR(255)NOT NULL,
-    reagents VARCHAR(255)NOT NULL,
-    wrong_products_1 VARCHAR(255)NOT NULL,
-    wrong_products_2 VARCHAR(255)NOT NULL,
-    wrong_products_3 VARCHAR(255)NOT NULL
-)
-
+    type_of_tasks VARCHAR(255)
+);
