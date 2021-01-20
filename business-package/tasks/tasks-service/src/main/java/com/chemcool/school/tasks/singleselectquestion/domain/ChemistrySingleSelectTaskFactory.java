6eb58bd348ce0a12405c7b7357572a546c144307
VolkovIO <volkovself@gmail.com>
2021-01-20 11:07:52 +0300

    Рефакторинг SingleSelectQuestion в оддельную папку, с логами, с тестами

--- business-package/tasks/tasks-service/src/main/java/com/chemcool/school/tasks/singleselectquestion/domain/ChemistrySingleSelectTaskFactory.java
+++ business-package/tasks/tasks-service/src/main/java/com/chemcool/school/tasks/singleselectquestion/domain/ChemistrySingleSelectTaskFactory.java
@@ -1,5 +1,6 @@
 package com.chemcool.school.tasks.singleselectquestion.domain;
 
+import com.chemcool.school.tasks.singleselectquestion.exeption.ChemistrySingleSelectTaskFormatException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -8,10 +9,10 @@ import org.springframework.stereotype.Component;
 public class ChemistrySingleSelectTaskFactory {
 
     public static ChemistrySingleSelectTask createTask(ChemistrySingleSelectTaskExample example){
-        //  Тут проверки на условия при создания задач
-        //  new ChemistrySingleSelectTaskFormatException("Описание ошибки");
-        //
-
+        
+        if (Integer.parseInt(example.getChapterIdExample()) < 1 || Integer.parseInt(example.getChapterIdExample()) > 4) {
+            throw new ChemistrySingleSelectTaskFormatException("Уровень химии не может быть меньше 1 или больше 4.");
+        }
         log.info("Попытка создать задачу с ВЫБОРОМ ФИКСИРОВАННОГО ОТВЕТА");
         return ChemistrySingleSelectTask.createChemistrySingleSelectTask(example);
     }
