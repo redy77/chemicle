0fe6d420fb183fdf04266dd3dd65326eb8ab8295
Mukhammad Jalolov <muhammadzalolov@gmail.com>
2021-01-27 05:18:16 +0700

    перенес сущности в chemmatches-service

---
+++ business-package/tasks/chemmatches/chemmatches-service/src/main/java/com/chemcool/school/chemmatches/TypeOfTask.java
@@ -0,0 +1,8 @@
+package com.chemcool.school.chemmatches;
+
+public enum TypeOfTask {
+    CHEMISTRY_EQUATION,
+    FIXED_ANSWER,
+    MATCHING,
+    SINGLE_SELECT
+}
---
+++ business-package/tasks/chemmatches/chemmatches-service/src/main/java/com/chemcool/school/chemmatches/domain/ChemistryMatchingTask.java
@@ -0,0 +1,47 @@
+package com.chemcool.school.chemmatches.domain;
+
+import com.chemcool.school.chemmatches.TypeOfTask;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "matching_task_of_two_sides")
+public class ChemistryMatchingTask {
+
+    @Id
+    @Column(name = "chemistry_matching_task_uuid")
+    private String chemistryMatchingTaskUuid;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "chapter_id")
+    private String chapterId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type_of_task")
+    private TypeOfTask typeOfTask;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    @JoinColumn(name = "question_id")
+    private List<CoupleForMatching> coupleForMatchingList;
+
+    public static ChemistryMatchingTask createChemistryMatchingTask(ChemistryMatchingTaskExample chemistryMatchingTaskExample) {
+        return new ChemistryMatchingTask(
+                UUID.randomUUID().toString(),
+                chemistryMatchingTaskExample.getDescription(),
+                chemistryMatchingTaskExample.getChapterId(),
+                chemistryMatchingTaskExample.getTypeOfTask(),
+                chemistryMatchingTaskExample.getCoupleForMatchingList()
+        );
+    }
+
+}
