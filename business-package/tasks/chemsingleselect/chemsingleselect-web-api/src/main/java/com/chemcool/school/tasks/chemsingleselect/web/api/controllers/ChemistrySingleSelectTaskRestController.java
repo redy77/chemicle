1b9274c12c50bb105be163bbc0e92ce11898506e
StanislavPal <stasunado@gmail.com>
2021-01-27 16:44:48 +0400

    Добился запуска модуля ChemSingleSelect со свагером после переноса. Работают РЕСТ запросы.

--- business-package/tasks/chemsingleselect/chemsingleselect-web-api/src/main/java/com/chemcool/school/tasks/chemsingleselect/web/api/controllers/ChemistrySingleSelectTaskRestController.java
+++ business-package/tasks/chemsingleselect/chemsingleselect-web-api/src/main/java/com/chemcool/school/tasks/chemsingleselect/web/api/controllers/ChemistrySingleSelectTaskRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tasks/single_select")
+@RequestMapping("/tasks")
 @RequiredArgsConstructor
 public class ChemistrySingleSelectTaskRestController {
 
