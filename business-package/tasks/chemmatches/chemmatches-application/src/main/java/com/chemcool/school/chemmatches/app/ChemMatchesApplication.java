6ab1d767a7ebf91257446a6cb0634b6dd1456d24
Mukhammad Jalolov <muhammadzalolov@gmail.com>
2021-01-27 21:37:46 +0700

    мелкие поправки

--- business-package/tasks/chemmatches/chemmatches-application/src/main/java/com/chemcool/school/chemmatches/app/ChemMatchesApplication.java
+++ business-package/tasks/chemmatches/chemmatches-application/src/main/java/com/chemcool/school/chemmatches/app/ChemMatchesApplication.java
@@ -1,6 +1,7 @@
 package com.chemcool.school.chemmatches.app;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -13,4 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("com.chemcool.school.chemmatches")
 @EnableEurekaClient
 public class ChemMatchesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChemMatchesApplication.class, args);
+    }
 }
