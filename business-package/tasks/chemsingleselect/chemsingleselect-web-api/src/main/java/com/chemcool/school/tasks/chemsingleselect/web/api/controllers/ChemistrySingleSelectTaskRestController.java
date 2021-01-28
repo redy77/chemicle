145759bc0295de735ebc7a3422a958da541490a1
Stanislav <paliutin.stanislav@gmail.com>
2021-01-28 04:17:36 +0400

    правка ендпоинта

--- business-package/tasks/chemsingleselect/chemsingleselect-web-api/src/main/java/com/chemcool/school/tasks/chemsingleselect/web/api/controllers/ChemistrySingleSelectTaskRestController.java
+++ business-package/tasks/chemsingleselect/chemsingleselect-web-api/src/main/java/com/chemcool/school/tasks/chemsingleselect/web/api/controllers/ChemistrySingleSelectTaskRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1.0/tasks/single_select")
+@RequestMapping("/v1/tasks/single-select")
 @RequiredArgsConstructor
 public class ChemistrySingleSelectTaskRestController {
 
