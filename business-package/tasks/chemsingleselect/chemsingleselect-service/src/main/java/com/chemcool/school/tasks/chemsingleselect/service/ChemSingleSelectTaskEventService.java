98686fb62b8c9418ee5e0e89d931f55d375cd87d
StanislavPal <stasunado@gmail.com>
2021-01-29 19:34:40 +0400

    добавил сервисный слой для кафки

--- business-package/tasks/chemsingleselect/chemsingleselect-service/src/main/java/com/chemcool/school/tasks/chemsingleselect/service/ChemSingleSelectTaskEventService.java
+++ business-package/tasks/chemsingleselect/chemsingleselect-service/src/main/java/com/chemcool/school/tasks/chemsingleselect/service/ChemSingleSelectTaskEventService.java
@@ -2,7 +2,9 @@ package com.chemcool.school.tasks.chemsingleselect.service;
 
 import com.chemcool.school.tasks.chemsingleselect.domain.ChemSingleSelectTask;
 import com.chemcool.school.tasks.chemsingleselect.domain.ChemSingleSelectTaskEvent;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ChemSingleSelectTaskEventService {
     public void save(ChemSingleSelectTask task) {
         //todo написать реализацию
