4f406ecefbf90165827ed32d6a216d3025e2abf7
irelion <irelion@mail.ru>
2021-02-07 19:07:09 +0300

    добавлен тест

--- business-package/tasks/chemfixedanswer/chemfixedanswer-web-api/src/test/java/com/chemcool/school/tasks/chemfixedanswer/web/api/controllers/ChemFixedAnswerTaskRestControllerTest.java
+++ business-package/tasks/chemfixedanswer/chemfixedanswer-web-api/src/test/java/com/chemcool/school/tasks/chemfixedanswer/web/api/controllers/ChemFixedAnswerTaskRestControllerTest.java
@@ -1,24 +1,21 @@
 package com.chemcool.school.tasks.chemfixedanswer.web.api.controllers;
 
-import static com.github.restdriver.serverdriver.Matchers.*;
-import static com.github.restdriver.serverdriver.RestServerDriver.*;
-import static org.hamcrest.MatcherAssert.*;
-
+import com.github.restdriver.serverdriver.http.response.Response;
+import junit.framework.TestCase;
 import org.junit.Test;
 
-import com.github.restdriver.serverdriver.http.response.Response;
+import static com.github.restdriver.serverdriver.Matchers.hasStatusCode;
+import static com.github.restdriver.serverdriver.RestServerDriver.get;
+import static com.github.restdriver.serverdriver.RestServerDriver.header;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-class ChemFixedAnswerTaskRestControllerTest {
+public class ChemFixedAnswerTaskRestControllerTest extends TestCase {
 
     private static final String BASE_URL = "http://localhost:8080//tasks-chemfixedanswer-application/v.1.0/tasks/fixed-answer";
 
     @Test
-    public void getAllTasksJsonResponse(){
+    public void testGetAllTasksJsonResponse(){
         Response response = get(BASE_URL + "/", header("Accept", "application/json"));
         assertThat(response, hasStatusCode(200));
     }
-
-//    @Test
-//    public void
-
 }
