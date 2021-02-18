119fc70dbb6d844df247fe5de82340f1bfc2ba68
shch-dev <ivan.poleshchuk.russia@gmail.com>
2021-02-18 18:08:27 +0300

    скопирован модуль web-api

---
+++ business-package/tasks/chemequations/chemequations-web-api/src/main/java/com/chemcool/school/tasks/chemequations/web/api/exeption/ChemistryTaskEmptyException.java
@@ -0,0 +1,7 @@
+package com.chemcool.school.tasks.chemequations.web.api.exeption;
+
+public class ChemistryTaskEmptyException extends RuntimeException{
+    public ChemistryTaskEmptyException(String message) {
+        super(message);
+    }
+}
