16cc850d9d224ddbb642fa16c59bffcd04053c86
Azamat <Xrupsarius0505>
2021-02-22 00:19:57 +0300

    Внес дополнительные изменения для устранения ошибок.

--- api-gateway/web-api-gateway/web-api-gateway-application/src/main/java/com/chemcool/school/apigateway/WebApiGatewayApplication.java
+++ api-gateway/web-api-gateway/web-api-gateway-application/src/main/java/com/chemcool/school/apigateway/WebApiGatewayApplication.java
@@ -5,13 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.retry.annotation.EnableRetry;
 
-@SpringBootApplication(
-        scanBasePackages = {"com.chemcool.school.apigateway"}
-)
+@SpringBootApplication(scanBasePackages = {"com.chemcool.school.apigateway"})
 @EnableEurekaClient
 @EnableZuulProxy
 @ConfigurationPropertiesScan("com.chemcool.school.apigateway")
+@EnableRetry
 public class WebApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApiGatewayApplication.class, args);
