b51ea05bd058a7fb484fb1a5cd78c8905fb1977f
wizzard1177 <zheka1177@mail.ru>
2021-02-24 13:58:38 +0300

    Переработан метод сложения элементов

--- business-package/tasks/chemequations/chemequations-service/src/main/java/com/chemcool/school/tasks/chemequations/domain/ChemCompound.java
+++ business-package/tasks/chemequations/chemequations-service/src/main/java/com/chemcool/school/tasks/chemequations/domain/ChemCompound.java
@@ -1,5 +1,6 @@
 package com.chemcool.school.tasks.chemequations.domain;
 
+import com.chemcool.school.tasks.chemequations.exception.ChemEquationsTaskException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,25 +9,40 @@ import java.util.*;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class ChemCompound {
 
     private String compoundSymbols;
     private Map<ChemElement,Integer> compoundElements;
 
-    public boolean reaction(ChemElement ... elements){
-        if(compoundElements==null){
-            compoundElements = new HashMap();
-        }
-        for (ChemElement el : elements) {
-            if (!compoundElements.containsKey(el)){
-                compoundElements.put(el,1);
+    public ChemCompound() {
+        this.compoundElements = new HashMap();
+        this.compoundSymbols="";
+    }
 
-            }else {
-                compoundElements.put(el,compoundElements.get(el)+1);
-            }
-        }
-        compoundSymbols = compoundElements.toString();
+    public boolean checkFactors(){
         return true;
+    }
+
+    public void reaction(ChemElement ... elements){
+//        for (ChemElement el : elements) {
+//            if (!compoundElements.containsKey(el)){
+//                compoundElements.put(el,1);
+//
+//            }else {
+//                compoundElements.put(el,compoundElements.get(el)+1);
+//            }
+//        }
+        Arrays.stream(elements).forEach(e ->compoundElements.put(e,compoundElements.getOrDefault(e,0)+1));
+        if(checkFactors()){
+            reactionWrite();
+        } else {
+            throw new ChemEquationsTaskException("Ошибка условий формарования молекулы");
+        }
+
+    }
+
+    public void reactionWrite(){
+        compoundElements.keySet().stream().forEach(e->compoundSymbols+=e.getElementSymbol()+compoundElements.get(e));
+        compoundSymbols = compoundSymbols.replaceAll("1","");
     }
 }
