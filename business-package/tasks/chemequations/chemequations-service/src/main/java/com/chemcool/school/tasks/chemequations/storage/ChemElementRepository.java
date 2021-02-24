ee0ed2cb9f345615c9af1a609e9f01fe18cb7408
wizzard1177 <zheka1177@mail.ru>
2021-02-24 15:14:13 +0300

    переработан контроллер

--- business-package/tasks/chemequations/chemequations-service/src/main/java/com/chemcool/school/tasks/chemequations/storage/ChemElementRepository.java
+++ business-package/tasks/chemequations/chemequations-service/src/main/java/com/chemcool/school/tasks/chemequations/storage/ChemElementRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ChemElementRepository extends JpaRepository<ChemElement,String>, JpaSpecificationExecutor<ChemElement> {
+    Optional<ChemElement> findByElementNumber(int elementNumber);
 }
