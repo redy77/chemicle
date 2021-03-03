ca16034e0dae745fbc23bffa91b8e6ab9045eac5
wizzard1177 <zheka1177@mail.ru>
2021-03-03 15:00:29 +0300

    бета2

--- business-package/tasks/chemequations/chemequations-service/src/main/java/com/chemcool/school/tasks/chemequations/service/ChemEquationsTaskAnswerCheckService.java
+++ business-package/tasks/chemequations/chemequations-service/src/main/java/com/chemcool/school/tasks/chemequations/service/ChemEquationsTaskAnswerCheckService.java
@@ -1,77 +1,83 @@
 package com.chemcool.school.tasks.chemequations.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+@Slf4j
 @Service
 public class ChemEquationsTaskAnswerCheckService {
-    private String bufRightAnswer;
-    private String bufUserAnswer;
+    private boolean[] result;
+    private Set<String> bufRightAnswer;
+    private Set<String> bufUserAnswer;
 
     public static void main(String[] args) {
-        String r = "CuSO4 + 2NaOH → Cu(OH)2↓+ Na2SO4";
-        String u = "CuSO4 + 2NaOH → u(OH)2↓+ Na2SO4";
+        String trueAnswer = "CuSO4 + 2NaOH → Cu(OH)2↓+ Na2SO4↑";
+        String userAnswer = "CuSO4 + 2NaOH → Na2SO4↑ + Cu(OH)2↓";
         ChemEquationsTaskAnswerCheckService checker = new ChemEquationsTaskAnswerCheckService();
-        System.out.println(checker.checkAnswer(r.replaceAll(" ",""),u.replaceAll(" ","")));
+
+        System.out.println(Arrays.toString(checker.checkAnswer(
+                trueAnswer.replaceAll(" ", ""),
+                userAnswer.replaceAll(" ", "")))
+        );
     }
 
-    public boolean checkAnswer(String rightAnswer, String userAnswer) {
-        this.bufRightAnswer = rightAnswer;
-        this.bufUserAnswer = userAnswer;
-
-        //если все идеально верно
-        if (check()) {
-            System.out.println("Заебись, четко");
-            System.out.println(rightAnswer + " = " + userAnswer);
-            return true;
-        }
-
-        //если верно все кроме регистра элементов
-        if (checkRegister()) {
-            System.out.println("Некоректно прописаны элементы.");
-            System.out.println(rightAnswer + " != " + userAnswer);
-            return true;
-        }
-
-        // если проблемы с осадком
-        if(checkAgrStatus()) {
-            System.out.println("Ошибка в агрегатном состоянии продуктов реакции.");
-            System.out.println(rightAnswer + " != " + userAnswer);
-            return true;
-        }
-
-        //если верно все кроме знака
-        if (checkSymbol()) { // нужно допилить регулярку
-            System.out.println("Неверно указан вид реакции. (Знак между левой и правой частью уравнения)");
-            System.out.println(rightAnswer + " != " + userAnswer);
-            return true;
-        }
-
-        System.out.println("Что то не так");
-        System.out.println(bufRightAnswer +" != "+ bufUserAnswer );
-        return false;
+    public ChemEquationsTaskAnswerCheckService() {
+        this.result = new boolean[4];
+        this.bufRightAnswer = new TreeSet();
+        this.bufUserAnswer = new TreeSet();
     }
 
-    private boolean check(){
-        if (bufRightAnswer.equals(bufUserAnswer)) {
-            return true;
-        }
-        return false;
-    }
-    private boolean checkRegister(){
-        bufRightAnswer = bufRightAnswer.toLowerCase();
-        bufUserAnswer = bufUserAnswer.toLowerCase();
-        return check();
+    public boolean[] checkAnswer(String rightAnswer, String userAnswer) {
+
+        result[0] = check(rightAnswer, userAnswer);
+        log.info("Проверка уравнения без учета порядка: " + result[0]);
+
+        result[1] = checkRegister(rightAnswer, userAnswer);
+        log.info("Проверка уравнения без учета регистра: " + result[1]);
+
+        result[2] = checkAgrStatus(rightAnswer, userAnswer);
+        log.info("Проверка уравнения без агрегатного состояния: " + result[2]);
+
+        result[3] = checkSymbol(rightAnswer, userAnswer);
+        log.info("Проверка символа сравнения: " + result[3]);
+
+        return result;
     }
 
-    private boolean  checkSymbol(){
-        bufRightAnswer = bufRightAnswer.replaceAll("[→=⇄↔]","."); // нужно допилить регулярку
-        bufUserAnswer = bufUserAnswer.replaceAll("[→=⇄↔]",".");
-        return check();
+    //обнуляем буферные переменные
+    private void bufClear() {
+        bufUserAnswer.clear();
+        bufRightAnswer.clear();
     }
 
-    private boolean checkAgrStatus(){
-        bufRightAnswer = bufRightAnswer.replaceAll("[↓↑]",".."); // нужно допилить регулярку
-        bufUserAnswer = bufUserAnswer.replaceAll("[↓↑]","..");
-        return check();
+    //проверка элементов без учета порядка
+    private boolean check(String r, String u) {
+        bufClear();
+        bufRightAnswer.addAll(Arrays.asList(r.split("[→=⇄↔+]")));
+        bufUserAnswer.addAll(Arrays.asList(u.split("[→=⇄↔+]")));
+        return bufRightAnswer.equals(bufUserAnswer);
+    }
+
+    //без учета регистра
+    private boolean checkRegister(String r, String u) {
+        return check(r.toLowerCase(), u.toLowerCase());
+    }
+
+    //без учета агрегатного состояния продуктов реакции
+    private boolean checkAgrStatus(String r, String u) {
+        r = r.replaceAll("[↓↑]", "");
+        u = u.replaceAll("[↓↑]", "");
+        return check(r, u) || checkRegister(r, u);
+    }
+
+    //проверка вида реакции (знака между левой и правой частью уравнения)
+    private boolean checkSymbol(String r, String u) {
+        r = r.replaceAll("[^→=⇄↔]", "");
+        u = u.replaceAll("[^→=⇄↔]", "");
+        return r.equals(u);
     }
 }
