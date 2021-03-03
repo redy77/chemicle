51fa6a035bf422446fcc35c12476747cb6c35bc3
Alexander <Lazovski1991@gmail.com>
2021-03-03 18:59:59 +0300

    create frameword and add base dependencies

---
+++ business-package/answers-task/answers-task-application/src/main/java/com/chemcool/school/answerstask/app/AnswersTaskApplication.java
@@ -0,0 +1,13 @@
+package com.chemcool.school.answerstask.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnswersTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnswersTaskApplication.class, args);
+    }
+
+}
---
+++ business-package/answers-task/answers-task-application/src/test/java/com/chemcool/school/answerstask/app/AnswersTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chemcool.school.answerstask.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnswersTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
