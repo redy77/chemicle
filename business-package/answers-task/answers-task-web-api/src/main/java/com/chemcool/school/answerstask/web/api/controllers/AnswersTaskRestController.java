a9a72841f109be2a6708597626368635857d1028
Alexander <Lazovski1991@gmail.com>
2021-03-06 20:02:38 +0300

    send check bags

--- business-package/answers-task/answers-task-web-api/src/main/java/com/chemcool/school/answerstask/web/api/controllers/AnswersTaskRestController.java
+++ business-package/answers-task/answers-task-web-api/src/main/java/com/chemcool/school/answerstask/web/api/controllers/AnswersTaskRestController.java
@@ -1,20 +1,32 @@
 package com.chemcool.school.answerstask.web.api.controllers;
 
+import com.chemcool.school.answerstask.tasks.chemmatches.domain.CoupleForMatching;
+import com.chemcool.school.answerstask.web.api.domain.TaskType;
+import com.chemcool.school.answerstask.web.api.service.CheckUserAnswersService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v.1.0/answers-task")
 @RequiredArgsConstructor
 public class AnswersTaskRestController {
 
-    @GetMapping
-    @ApiOperation("Возвращает true или false ответ пользователю")
-    public boolean checkAnswerUser() {
-        return true;
-    }
+    private final CheckUserAnswersService checkUserAnswersService;
 
+    @PostMapping("/{taskId}")
+    @ApiOperation("Возвращает true или false ответ пользователю")
+    public boolean checkAnswerUser(@PathVariable String taskId, @RequestParam TaskType taskType,
+                                   @RequestParam(required = false) String userAnswers,
+                                   @RequestBody(required = false) CoupleForMatching coupleForMatching) {
+
+        boolean resultOfChecking = true;
+
+        if (userAnswers != null) {
+            resultOfChecking = checkUserAnswersService.checkUserAnswer(taskId, taskType, userAnswers);
+        } else {
+            resultOfChecking = checkUserAnswersService.checkUserAnswer(taskId, taskType, coupleForMatching);
+        }
+        return resultOfChecking;
+    }
 }
