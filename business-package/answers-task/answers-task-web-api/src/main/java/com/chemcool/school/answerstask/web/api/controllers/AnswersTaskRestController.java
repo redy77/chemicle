77f811ee0e00f23504c7be611c3223b28ec25bfe
Alexander <Lazovski1991@gmail.com>
2021-03-08 16:41:06 +0300

    add save users answers

--- business-package/answers-task/answers-task-web-api/src/main/java/com/chemcool/school/answerstask/web/api/controllers/AnswersTaskRestController.java
+++ business-package/answers-task/answers-task-web-api/src/main/java/com/chemcool/school/answerstask/web/api/controllers/AnswersTaskRestController.java
@@ -1,8 +1,11 @@
 package com.chemcool.school.answerstask.web.api.controllers;
 
+import com.chemcool.school.answerstask.domain.UserAnswersCorrect;
+import com.chemcool.school.answerstask.service.UserAnswersCorrectService;
 import com.chemcool.school.answerstask.tasks.chemmatches.domain.CoupleForMatching;
 import com.chemcool.school.answerstask.web.api.domain.TaskType;
 import com.chemcool.school.answerstask.web.api.service.CheckUserAnswersService;
+import com.chemcool.school.answerstask.web.api.service.UserIdentJwtParser;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,19 +18,24 @@ import java.util.List;
 public class AnswersTaskRestController {
 
     private final CheckUserAnswersService checkUserAnswersService;
+    private final UserIdentJwtParser jwtParser;
+    private final UserAnswersCorrectService userAnswersCorrectService;
 
     @PostMapping
     @ApiOperation("Возвращает true или false ответ пользователю")
     public boolean checkAnswerUser(@RequestParam String taskId, @RequestParam TaskType taskType,
                                    @RequestParam(required = false) String userAnswers,
-                                   @RequestBody(required = false) List<CoupleForMatching> coupleForMatchingList) {
+                                   @RequestBody(required = false) List<CoupleForMatching> coupleForMatchingList,
+                                   @RequestHeader(value = "AuthorizationToken") String token) {
 
         if (userAnswers != null) {
-            return checkUserAnswersService.checkUserAnswer(taskId, taskType, userAnswers);
+            boolean checkUserAnswers = checkUserAnswersService.checkUserAnswer(taskId, taskType, userAnswers);
+            UserAnswersCorrect userAnswersCorrect = new UserAnswersCorrect(jwtParser.getIdUserOfToken(token), jwtParser.getEmailUserOfToken(token), taskId);
+            userAnswersCorrectService.saveUserCorrectAnswers(userAnswersCorrect);
+            return checkUserAnswers;
+            //сдулать обработки ошибок при сохранении одинаковых ответов и при ненахождении ответа в бд
         } else {
             return checkUserAnswersService.checkUserAnswer(taskId, coupleForMatchingList);
         }
-
-        //можно попробовать в параметрах контролера вместо requestParam requestBody с Map<String, String> payload.
     }
 }
