a2c98e65ebb9a898db8a893db2d834c82a299652
Rusl Mag <se-box@inbox.ru>
2021-03-09 19:20:44 +0300

    удалил файлы фронта

--- security-package/react-template/Readme.md
+++
@@ -1,6 +0,0 @@
-в терминале вводим команды по очереди (для запуска в режиме разработки)
-
-```bash
-cd security-package/react-template
-npm install && npm start
-```
--- security-package/react-template/src/constants/index.js
+++
@@ -1,9 +0,0 @@
-export const API_AUTHENTICATION_URL = 'http://localhost:8080/authorization-application';
-export const API_REGISTRATION_URL = 'http://localhost:8080/registration-application';
-
-export const ACCESS_TOKEN = 'accessToken';
-
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
-
-export const GOOGLE_AUTH_URL = API_AUTHENTICATION_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
-export const FACEBOOK_AUTH_URL = API_AUTHENTICATION_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
