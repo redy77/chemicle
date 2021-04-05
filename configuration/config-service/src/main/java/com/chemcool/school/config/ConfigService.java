b2b680b0cd6f73704ec860ad2cd82e6a9adaac8a
patric <13bombom666@mail.ru>
2021-04-05 16:06:01 +0300

    Решение конфликтов 4

--- configuration/config-service/src/main/java/com/chemcool/school/config/ConfigService.java
+++ configuration/config-service/src/main/java/com/chemcool/school/config/ConfigService.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+@SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
-@SpringBootApplication(scanBasePackages = {"com.chemcool.school"})
 public class ConfigService {
     public static void main(String[] args) {
         SpringApplication.run(ConfigService.class, args);
