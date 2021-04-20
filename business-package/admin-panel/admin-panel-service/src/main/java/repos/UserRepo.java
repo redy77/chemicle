fe2b4fb498cef3599d912cdcddf01cbd83053405
rudikone <mudoda51>
2021-04-20 20:14:10 +0300

    удалил admin panel

--- business-package/admin-panel/admin-panel-service/src/main/java/repos/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package repos;
-
-
-import domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
