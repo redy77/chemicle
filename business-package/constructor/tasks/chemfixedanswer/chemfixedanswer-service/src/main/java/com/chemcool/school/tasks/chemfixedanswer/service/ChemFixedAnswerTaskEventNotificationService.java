4c5270c439bf6cafecaf3ed7ee6b9e0545be3ce2
Dmitrii R <rojoc@yandex.ru>
2021-05-01 18:07:51 +0300

    исправил конфликты + текущие изменения в проекте

--- business-package/constructor/tasks/chemfixedanswer/chemfixedanswer-service/src/main/java/com/chemcool/school/tasks/chemfixedanswer/service/ChemFixedAnswerTaskEventNotificationService.java
+++
@@ -1,7 +0,0 @@
-package com.chemcool.school.tasks.chemfixedanswer.service;
-
-import com.chemcool.school.tasks.chemfixedanswer.domain.ChemFixedAnswerTaskEvent;
-
-public interface ChemFixedAnswerTaskEventNotificationService {
-    void send(ChemFixedAnswerTaskEvent event);
-}
