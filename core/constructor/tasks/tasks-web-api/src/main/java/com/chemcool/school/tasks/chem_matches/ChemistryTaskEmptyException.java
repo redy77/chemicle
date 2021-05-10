91379af6f2abc2c8314ad27b3cc0bbaa6e945aaf
Dmitrii R <rojoc@yandex.ru>
2021-05-10 22:12:39 +0300

    Переместил файлы (версия 2)

---
+++ core/constructor/tasks/tasks-web-api/src/main/java/com/chemcool/school/tasks/chem_matches/ChemistryTaskEmptyException.java
@@ -0,0 +1,7 @@
+package com.chemcool.school.tasks.chem_matches;
+
+public class ChemistryTaskEmptyException extends RuntimeException{
+    public ChemistryTaskEmptyException(String message) {
+        super(message);
+    }
+}
