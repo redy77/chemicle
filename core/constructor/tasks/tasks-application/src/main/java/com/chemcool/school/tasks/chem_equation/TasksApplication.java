de31996fe359b7035b508147b2ebb4e368dea5d5
Dmitrii R <rojoc@yandex.ru>
2021-05-12 15:31:57 +0300

    Убрал коментарии с тестов

--- core/constructor/tasks/tasks-application/src/main/java/com/chemcool/school/tasks/chem_equation/TasksApplication.java
+++ core/constructor/tasks/tasks-application/src/main/java/com/chemcool/school/tasks/chem_equation/TasksApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = {"com.chemcool.school.tasks"})
 @EntityScan("com.chemcool.school.tasks")
--- core/constructor/tasks/tasks-web-api/src/main/java/com/chemcool/school/tasks/infrastructure/configuration/SwaggerConfig.java
+++ core/constructor/tasks/tasks-web-api/src/main/java/com/chemcool/school/tasks/infrastructure/configuration/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.chemcool.school.tasks.chem_matches.infrastructure.configuration;
+package com.chemcool.school.tasks.infrastructure.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
