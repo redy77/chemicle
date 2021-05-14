c2ea9910d9a2102e2cc6574707b235c24c1cfca1
Dmitrii R <rojoc@yandex.ru>
2021-05-14 14:19:33 +0300

    рефакторинг 3.

--- core/constructor/tasks/tasks-application/src/main/java/com/chemcool/school/tasks/app/TasksApplication.java
+++ core/constructor/tasks/tasks-application/src/main/java/com/chemcool/school/tasks/app/TasksApplication.java
@@ -1,4 +1,4 @@
-package com.chemcool.school.tasks.chemEquation;
+package com.chemcool.school.tasks.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
