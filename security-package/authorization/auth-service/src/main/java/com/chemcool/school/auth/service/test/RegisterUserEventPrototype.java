514cd6428cd8f4a4bc4b101cedc8ce4f3a76511f
Elena <aeglena@mail.ru>
2021-05-26 22:14:03 +0900

    тесты на проверку

---
+++ security-package/authorization/auth-service/src/main/java/com/chemcool/school/auth/service/test/RegisterUserEventPrototype.java
@@ -0,0 +1,15 @@
+package com.chemcool.school.auth.service.test;
+
+import com.chemcool.school.auth.service.domain.RegisterUser;
+import com.chemcool.school.auth.service.domain.RegisterUserEvent;
+
+public class RegisterUserEventPrototype {
+    public static RegisterUserEvent aRegisterUserEvent(){
+        RegisterUserEvent rUserEvent = new RegisterUserEvent();
+        rUserEvent.setEventId("test_eventId");
+        rUserEvent.setEventVersion("test_eventVersion");
+        rUserEvent.setAuthorId("test_authorId");
+        rUserEvent.setOccurringContext("test_occurringContext");
+        return rUserEvent;
+    }
+}
