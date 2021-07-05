f3943ba83cc78b9b1edfb0c4b7ab699065d99b42
ViacheslavP <public.viacheslav@gmail.com>
2021-07-05 19:42:46 +0300

    Переместил тесты в  app tests. Работает

--- security-package/authorization/auth-app/src/test/java/com/chemcool/school/auth/test/RegisterUserEventNotificationServiceImplTest.java
+++ security-package/authorization/auth-app/src/test/java/com/chemcool/school/auth/test/RegisterUserEventNotificationServiceImplTest.java
@@ -1,11 +1,10 @@
-package com.chemcool.school.auth.service.test;
+package com.chemcool.school.auth.test;
 import com.chemcool.school.auth.service.domain.RegisterUserEvent;
 import com.chemcool.school.auth.service.service.RegisterUserEventNotificationServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static com.chemcool.school.auth.service.test.RegisterUserEventPrototype.aRegisterUserEvent;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.*;
 
@@ -14,7 +13,7 @@ class RegisterUserEventNotificationServiceImplTest {
     private RegisterUserEventNotificationServiceImpl registerUserEventNotificationService=mock(RegisterUserEventNotificationServiceImpl.class);
 @Test
     void send(){
-        registerUserEventNotificationService.send(aRegisterUserEvent());
+        registerUserEventNotificationService.send(RegisterUserEventPrototype.aRegisterUserEvent());
         verify(registerUserEventNotificationService, times(1)).send(isA(RegisterUserEvent.class));
 
     }
--- security-package/authorization/auth-app/src/test/java/com/chemcool/school/auth/test/UserRegistrationEventServiceImplTest.java
+++ security-package/authorization/auth-app/src/test/java/com/chemcool/school/auth/test/UserRegistrationEventServiceImplTest.java
@@ -1,13 +1,11 @@
-package com.chemcool.school.auth.service.test;
+package com.chemcool.school.auth.test;
 
 import com.chemcool.school.auth.service.domain.RegisterUserEvent;
 import com.chemcool.school.auth.service.service.UserRegistrationEventServiceImpl;
-import com.chemcool.school.auth.service.storage.RegisterUserEventRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static com.chemcool.school.auth.service.test.RegisterUserEventPrototype.aRegisterUserEvent;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -16,7 +14,7 @@ class UserRegistrationEventServiceImplTest {
     @Test
     void saveEvent(){
         UserRegistrationEventServiceImpl registerUserEventService= mock(UserRegistrationEventServiceImpl.class);
-        registerUserEventService.saveEvent(aRegisterUserEvent());
+        registerUserEventService.saveEvent(RegisterUserEventPrototype.aRegisterUserEvent());
         verify(registerUserEventService, times(1)).saveEvent(isA(RegisterUserEvent.class));
     }
 }
