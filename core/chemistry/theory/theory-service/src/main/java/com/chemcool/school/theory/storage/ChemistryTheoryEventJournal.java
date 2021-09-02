1649facc106e4f675bed93c4ce05bfacd9e538df
Alexander_Malhin <sonicmalhin@gmail.com>
2021-09-02 21:31:13 +0300

    theory очищена от кода (кроме настроек кафки)

--- core/chemistry/theory/theory-service/src/main/java/com/chemcool/school/theory/storage/ChemistryTheoryEventJournal.java
+++
@@ -1,7 +0,0 @@
-package com.chemcool.school.theory.storage;
-
-import com.chemcool.school.theory.domain.ChemistryTheoryEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChemistryTheoryEventJournal extends JpaRepository<ChemistryTheoryEvent, String> {
-}
