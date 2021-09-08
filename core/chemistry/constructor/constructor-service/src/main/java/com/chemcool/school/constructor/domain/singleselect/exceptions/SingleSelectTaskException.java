56cbb11d75b644e8c67cb2000570f1d46937ab46
Александр <kaizerr@bk.ru>
2021-09-08 18:39:46 +0300

    exceptions v2 added

--- core/chemistry/constructor/constructor-service/src/main/java/com/chemcool/school/constructor/domain/singleselect/exceptions/SingleSelectTaskException.java
+++
@@ -1,7 +0,0 @@
-package com.chemcool.school.constructor.domain.singleselect.exceptions;
-
-public class SingleSelectTaskException extends RuntimeException {
-    public SingleSelectTaskException(String message) {
-        super(message);
-    }
-}
