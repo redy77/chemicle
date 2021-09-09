42751917b096a75ad3a5adea23a7cc2cd5785164
faraponoff <faraponoff@mail.ru>
2021-09-09 19:21:30 +0300

    Theory-web-api and Task-web-api were successfully  deleted (Card#35, Card#36)

--- core/chemistry/tasks/tasks-web-api/src/main/java/com/chemcool/school/tasks/ChemEquationsAnswerException.java
+++
@@ -1,7 +0,0 @@
-package com.chemcool.school.tasks;
-
-public class ChemEquationsAnswerException extends RuntimeException {
-    public ChemEquationsAnswerException(String message) {
-        super(message);
-    }
-}
