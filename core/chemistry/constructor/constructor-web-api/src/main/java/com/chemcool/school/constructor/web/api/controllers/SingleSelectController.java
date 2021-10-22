d8467a25212fe70814cff28365d1797ba17967ce
ConstantineLee <rhowbn@gmail.com>
2021-10-22 17:21:10 +0300

    добавил валидацию дто

--- core/chemistry/constructor/constructor-web-api/src/main/java/com/chemcool/school/constructor/web/api/controllers/SingleSelectController.java
+++ core/chemistry/constructor/constructor-web-api/src/main/java/com/chemcool/school/constructor/web/api/controllers/SingleSelectController.java
@@ -11,17 +11,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/v1/single-select")
 @RequiredArgsConstructor
 public class SingleSelectController {
 
     private final SingleSelectService service;
-    private final SingleSelectConverter convertor;
+    private final SingleSelectConverter converter;
 
     @PostMapping("/build")
-    public ResponseEntity<String> build(@RequestBody SingleSelectDto dto) {
-        SingleSelectRepresentation representation = convertor.convertToRepresentation(dto);
+    public ResponseEntity<String> build(@RequestBody @Valid SingleSelectDto dto) {
+        SingleSelectRepresentation representation = converter.convertToRepresentation(dto);
         return ResponseEntity.ok(service.save(representation));
     }
 }
+
--- core/chemistry/constructor/constructor-web-api/src/main/java/com/chemcool/school/constructor/web/api/dto/SingleSelectDto.java
+++ core/chemistry/constructor/constructor-web-api/src/main/java/com/chemcool/school/constructor/web/api/dto/SingleSelectDto.java
@@ -1,19 +1,34 @@
 package com.chemcool.school.constructor.web.api.dto;
+
 /**
  * @Author Constantine Lee
  */
 
-import com.chemcool.school.constructor.domain.representation.SingleSelectRepresentation;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 public class SingleSelectDto {
+
+    @NotBlank
     private String conditionOfTask;
+
+    @NotBlank
     private String rightAnswer;
+
+    @NotNull
     private List<String> answers;
+
+    @Range(min = 1, max = 11)
     private Short classNum;
+
+    @Range(min = 1, max = 4)
     private Short chapterNum;
+
+    @NotBlank
     private Short paragraphNum;
 }
