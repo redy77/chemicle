391db40aa97438d6414c5320c794b29da4eeacae
redy77 <redy@yandex.ru>
2021-10-31 17:36:08 +0300

    удалил dto из сервиса таск

--- core/chemistry/tasks/tasks-service/src/main/java/com/chemcool/school/tasks/domain/exceptions/TaskNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.chemcool.school.tasks.domain.exceptions;
-
-public class TaskNotFoundException extends Exception {
-    public TaskNotFoundException(String taskId) {
-        super("Task with id " + taskId + " not found");
-    }
-}
--- core/chemistry/tasks/tasks-service/src/main/java/com/chemcool/school/tasks/domain/exceptions/TaskOfTypeNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.chemcool.school.tasks.domain.exceptions;
-
-import com.chemcool.school.tasks.statuses.TaskType;
-
-public class TaskOfTypeNotFoundException extends Exception{
-    public TaskOfTypeNotFoundException(TaskType type) {
-        super("No " + type.toString() + " tasks found");
-    }
-}
